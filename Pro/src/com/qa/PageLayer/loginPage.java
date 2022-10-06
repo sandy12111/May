3cee623dc3c142a061e37678fa4db5ba3af12499
sandy12111 <ganjewarsandesh321@gmail.com>
2022-10-06 10:07:49 +0530

    project
    GIT

---
+++ Pro/src/com/qa/PageLayer/loginPage.java
@@ -0,0 +1,11 @@
+package com.qa.PageLayer;
+
+public class loginPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("login page");
+
+	}
+
+}
